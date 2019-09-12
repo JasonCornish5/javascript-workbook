106b4972d30736648b34f4df924ee64ed55a25f9
JasonCornish5 <jason.cornish5@yahoo.com>
2019-09-11 20:45:57 -0500

    finished

--- 05week/spaceTravelToMars.js
+++ 05week/spaceTravelToMars.js
@@ -9,7 +9,35 @@ let jobTypes = {
   programmer: 'Any Ship!'
 };
 
-// Your code here
+class CrewMember {
+  constructor(name, job, specialSkill, ship) {
+    this.name = name;
+    this.job = job;
+    this.specialSkill = specialSkill;
+    this.ship = ship;
+  }
+  enterShip(shipName) {
+    this.ship = shipName;
+    shipName.crew.push(this);
+  }
+}
+
+class Ship {
+  constructor (name, type, ability, crew) {
+    this.name = name;
+    this.type = type;
+    this.ability = ability;
+    this.crew = [];
+  }
+  missionStatement() {
+    if (this.crew.length >= 1) {
+      return this.ability;
+    } else {
+      return "Can't perform a mission yet."
+    }
+  } 
+}
+
 
 //tests
 if (typeof describe === 'function'){
